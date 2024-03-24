290e838780c77370654cc553ed8c8b4948bf4e86
hakimihamzan <hakimihamzan20@gmail.com>
2024-03-24 21:59:03 +0800

    add title and meta-description to head

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from './Navbar'
 import { Inter } from "next/font/google"
+import Head from 'next/head';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,6 +11,10 @@ const inter = Inter({ subsets: ["latin"] })
 function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Head>
+        <title>Hakimi Hamzan</title>
+        <meta name="description" content="A software developer with keen eyes for great design. From front end development to back end development, I'll do them all." />
+      </Head>
       <Navbar />
       <main className={`${inter.className} mt-20 p-4 md:p-10`}>{children}</main>
     </>
